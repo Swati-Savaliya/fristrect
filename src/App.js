f9474ec67e58afe74a93a33c414e42ddd08479e2
Swati-Savaliya <Swatisavaliya09@gmail.com>
2024-02-28 08:26:14 +0530

    home work done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React + clone
+          Learn React + clone home work Done
         </a>
       </header>
     </div>
